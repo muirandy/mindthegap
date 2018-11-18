02903448a6cfb593be4e24dcaddd8e193c1e06e9
Andy Muir <andy.muir@aimyourtechnology.com>
2018-11-18 21:39:33 +0000

    Added Service.

---
+++ src/main/java/com/github/muirandy/mindthegap/repositories/AppRepository.java
@@ -0,0 +1,7 @@
+package com.github.muirandy.mindthegap.repositories;
+
+import java.util.List;
+
+public interface AppRepository {
+    List<StoredData> retrieveAllData();
+}
---
+++ src/main/java/com/github/muirandy/mindthegap/repositories/StoredData.java
@@ -0,0 +1,29 @@
+package com.github.muirandy.mindthegap.repositories;
+
+import org.apache.commons.lang3.builder.EqualsBuilder;
+import org.apache.commons.lang3.builder.HashCodeBuilder;
+import org.apache.commons.lang3.builder.ToStringBuilder;
+import org.apache.commons.lang3.builder.ToStringStyle;
+
+public class StoredData {
+    public String dataName;
+
+    public StoredData(String dataName) {
+        this.dataName = dataName;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        return EqualsBuilder.reflectionEquals(this, o);
+    }
+
+    @Override
+    public int hashCode() {
+        return HashCodeBuilder.reflectionHashCode(this);
+    }
+
+    @Override
+    public String toString() {
+        return ToStringBuilder.reflectionToString(this, ToStringStyle.MULTI_LINE_STYLE);
+    }
+}
